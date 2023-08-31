044318e44391a4fa06eb8234fd1e6d790293ddd9
Oksana <ereminaoksana82@yandex.ru>
2023-08-31 15:43:51 +0400

    Lesson4

---
+++ less4/checker_ssh.py
@@ -0,0 +1,27 @@
+import paramiko
+
+
+def ssh_checkout(host, user, passwd, cmd, text, port=22):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(hostname=host, username=user, password=passwd, port=port)
+    stdin, stdout, stderr = client.exec_command(cmd)
+    exit_code = stdout.channel.recv_exit_status()
+    out = (stdout.read() + stderr.read()).decode('utf-8')
+    client.close()
+    if text in out and exit_code == 0:
+        return True
+    else:
+        return False
+
+
+def upload_files(host, user, passwd, local_path, remote_path, port=22):
+    print(f"Загружаем файл {local_path} в каталог {remote_path}")
+    transport = paramiko.Transport((host, port))
+    transport.connect(None, username=user, password=passwd)
+    stpf = paramiko.SFTPClient.from_transport(transport)
+    stpf.put(local_path, remote_path)
+    if stpf:
+        stpf.close()
+    if transport:
+        transport.close()
